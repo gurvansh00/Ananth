44c5cf7972756e6dbfc3b740a5412e31b6a0dff9
AnanyaD <75618420+AnanyaKD@users.noreply.github.com>
2024-05-06 18:46:10 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -35,24 +35,24 @@ def search(pr):
 tab1,tab2 = st.tabs(["HOME","LEARN"])
 
 with tab1:
-    homecol1,homecol2 = st.columns([0.7,0.3])
-    homecol2.image("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG")
+   homecol1,homecol2 = st.columns([0.7,0.3])
+    homecol2.image("https://static.vecteezy.com/system/resources/previews/002/396/557/non_2x/reduce-reduce-recycle-free-vector.jpg")
     st.header("AI for Earth Waste Management")
     st.write("Welcome to AI for Earth Waste Management!")
-    st.markdown(
-    """
-    <style>
-    body {
-        background-image: url('d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG');
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-    if uploaded_image is not None:
+    homecol1.subheader("How It Works")
+    homecol1.markdown("""
+                      - Upload the clicked image
+                      - wait for the AI to do its magic
+                      - Match the material type with the Object
+                      - Choose the option from Reuse, Recycle or Reduce
+                      """)
+     homecol1.markdown("""
+                      - Upload the clicked image
+                      - wait for the AI to do its magic
+                      - Match the material type with the Object
+                      - Choose the option from Reuse, Recycle or Reduce
+                      """)
+if uploaded_image is not None:
         img = Image.open(uploaded_image)
         st.image(img)
         col1,col2 = st.columns(2)
