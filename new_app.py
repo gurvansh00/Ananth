0dfe7ee94b0cab6301a4de34ba8b76de6d8f67f6
AnanyaD <75618420+AnanyaKD@users.noreply.github.com>
2024-05-06 20:06:06 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -37,17 +37,17 @@ def search(pr):
 tab1,tab2 = st.tabs(["HOME","LEARN"])
 
 with tab1:
-    homecol1,homecol2 = st.columns([0.7,0.3])
-    homecol2.image("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG")
-    st.header("AI for Earth Waste Management")
-    st.write("Welcome to AI for Earth Waste Management!")
-    homecol1.subheader("How It Works")
-    homecol1.markdown("""
-                      - Upload the clicked image
-                      - wait for the AI to do its magic
-                      - Match the material type with the Object
-                      - Choose the option from Reuse, Recycle or Reduce
-                      """)
+	homecol1,homecol2 = st.columns([0.7,0.3])
+	homecol2.image("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG")
+	st.header("AI for Earth Waste Management")
+	st.write("Welcome to AI for Earth Waste Management!")
+	homecol1.subheader("How It Works")
+	homecol1.markdown("""
+		      - Upload the clicked image
+		      - wait for the AI to do its magic
+		      - Match the material type with the Object
+		      - Choose the option from Reuse, Recycle or Reduce
+		      """)
 	uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 	if uploaded_image is not None:
 		img = Image.open(uploaded_image)
