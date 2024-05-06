553dbae020065d0da4a97bf61e81c7fe4660390d
AnanyaD <75618420+AnanyaKD@users.noreply.github.com>
2024-05-06 18:27:07 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -36,7 +36,14 @@ def search(pr):
 tab1,tab2 = st.tabs(["HOME","LEARN"])
 
 with tab1:
-    st.markdown('<style>body {background-image: url("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG"); background-size: cover;}</style>', unsafe_allow_html=True)
+    st.markdown("""
+                <style>
+    .stApp {
+    background-image: url("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG");
+    background-size: cover;
+    }
+    </style>
+                """, unsafe_allow_html = True)
     homecol1,homecol2 = st.columns([0.7,0.3])
     homecol2.image("d3df85a0-600f-41f1-9ad3-31da745a9e1d.JPG")
     st.header("AI for Earth Waste Management")
